2930f57773b554f5b41125dd4daf96a5842d2806
allen <986847686@qq.com>
2022-01-27 01:29:04 +0800

    新建多模块项目

---
+++ flash-sale-web/src/main/java/com/zsy/flashsale/web/FlashSaleWebApplication.java
@@ -0,0 +1,17 @@
+package com.zsy.flashsale.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author Allenzsy
+ * @Date 2022/1/27 1:02
+ * @Description:
+ */
+@SpringBootApplication
+public class FlashSaleWebApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FlashSaleWebApplication.class);
+    }
+
+}
